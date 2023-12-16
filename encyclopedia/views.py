0f966c428040a9602c8fca59ed8c6a37745e05c1
Bruno <br_coelho98@hotmail.com>
2023-12-16 00:32:27 -0300

    Adicionado o Search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -28,3 +28,23 @@ def entry(request, entry):
         return render(request, "encyclopedia\error.html", {
             "title": entry
         })
+        
+def search(request):
+    if request.method == "POST":
+        value = request.POST['q']
+        html_content = md_to_html(value)
+        if html_content is not None:
+            return render(request, "encyclopedia\entry.html", {
+            "content": html_content,
+            "title": value
+        })
+        else:
+            entries = []
+            for entry in util.list_entries():
+                if value.upper() in entry.upper():
+                    entries.append(entry)
+            return render(request, "encyclopedia\search.html", {
+            "entries": entries,
+            "search": True,
+            "value": value
+            })
